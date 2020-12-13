46bddfcd0003b6e4f6e6337f8ed9516d43823b2b
Sharmin <sharmin@outlook.com.au>
2020-12-13 01:04:01 +0000

    Added colour-changing logic

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,47 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+function changeMode() {
+  const dark = (document.querySelectorAll("html")[0].style.backgroundColor !== 'rgb(237, 185, 209)' ? true : false);
+  if (dark) {
+    document.querySelectorAll("html")[0].style.backgroundColor = "#edb9d1";
+    if (document.querySelectorAll("h1").length != 0) {
+      document.querySelectorAll("h1").forEach(element => element.style.color = "#7d5169");
+    }
+    if (document.querySelectorAll("h2").length != 0) {
+      document.querySelectorAll("h2").forEach(element => element.style.color = "#7d5169");
+    }
+    if (document.querySelectorAll("h4").length != 0) {
+      document.querySelectorAll("h4").forEach(element => element.style.color = "#7d5169");
+    }
+    if (document.querySelectorAll("#info").length != 0) {
+      document.querySelectorAll("#info").forEach(element => element.style.background = "#deabca");
+      document.querySelectorAll("#info").forEach(element => element.style.borderColor = "#a8658e");
+    }
+    if (document.querySelectorAll("#nav").length != 0) {
+      document.querySelectorAll("a").forEach(element => element.style.borderColor = "#deabca");
+      document.querySelectorAll("#nav").forEach(element => element.style.backgroundColor = "#a8658e");
+    }
+  } else {
+    document.querySelectorAll("html")[0].style.backgroundColor = "#b9ceed";
+    if (document.querySelectorAll("h1").length != 0) {
+      document.querySelectorAll("h1").forEach(element => element.style.color = "#515e7d");
+    }
+    if (document.querySelectorAll("h2").length != 0) {
+      document.querySelectorAll("h2").forEach(element => element.style.color = "#515e7d");
+    }
+    if (document.querySelectorAll("h4").length != 0) {
+      document.querySelectorAll("h4").forEach(element => element.style.color = "#515e7d");
+    }
+    if (document.querySelectorAll("#info").length != 0) {
+      document.querySelectorAll("#info").forEach(element => element.style.background = "#abb9de");
+      document.querySelectorAll("#info").forEach(element => element.style.borderColor = "#6578a8");
+    }
+    if (document.querySelectorAll("#nav").length != 0) {
+      document.querySelectorAll("a").forEach(element => element.style.borderColor = "#abb9de");
+      document.querySelectorAll("#nav").forEach(element => element.style.backgroundColor = "#6578a8");
+    }
+  }
+  
+}
